ff9e80081617445688d653f2cd80fab750dd1fec
myfahmi21 <fahmiahmadi123@gmail.com>
2020-10-02 23:00:19 +0700

    update cropping photo on Perbarui Data Keluarga pag

--- resources/views/m-family-mgmt/update-family-data.blade.php
+++ resources/views/m-family-mgmt/update-family-data.blade.php
@@ -81,10 +81,17 @@
                             </div>
                             <div class="form-group">
                                 <label class="form-control-label">Foto Keluarga :</label>
-                                <div class="img-result-foto-keluarga">
-                                    <img id="blah"  class="cropped-foto-keluarga" style="margin-bottom:5px;border:solid 1px #c2cad8;" width="140" height="90" src="{{ url('photo/kk/'.$data['family']->photo) }}" onerror="this.src='{{ url('assets/global/img/no-profile.jpg') }}'"/>
+                                <div id="result"></div>
+                                <div class="img-result-foto-keluarga" style=" visibility: hidden; height: 0; ">
+                                    <img id="img-view" src="{{ url('photo/kk/'.$data['family']->photo) }}" style="width:100%;"/>
                                 </div>
-                                <input id="upload-img" name="photo" type="file" onchange="document.getElementById('blah').src = window.URL.createObjectURL(this.files[0])">
+                                <input id="data-satu" type="text" value='{"x":65.65868263473054,"y":0,"width":283.2706586826347,"height":279.1389221556886,"rotate":0,"scaleX":1,"scaleY":1}' />
+                                <input id="data-dua" type="text" value='{"left":0,"top":0,"width":334,"height":334,"naturalWidth":512,"naturalHeight":512}' />
+
+                                <!-- <div class="img-result-foto-keluarga">
+                                    <img id="blah"  class="cropped-foto-keluarga" style="margin-bottom:5px;border:solid 1px #c2cad8;" width="140" height="90" src="{{ url('photo/kk/'.$data['family']->photo) }}" onerror="this.src='{{ url('assets/global/img/no-profile.jpg') }}'"/>
+                                </div> -->
+                                <input id="upload-img" name="photo_text" type="file" onchange="document.getElementById('blah').src = window.URL.createObjectURL(this.files[0])">
                                 <main class="page-foto-keluarga">
                                     <div class="box-2-foto-keluarga">
                                         <div class="result-foto-keluarga"></div>
@@ -101,7 +108,7 @@
                     <div class="row">
                         <div class="col-xl-12 col-md-12 m-b-10px text-right">
                             <input type="hidden" name="family_id" value="{{ $data['family']->id }}"/>
-                            <input id="upload-img-text" name="photo_text" type="hidden">
+                            <input id="upload-img-text" name="" type="hidden">
                             <a href="{{ url('family-management') }}" class="btn btn-primary pull-left">Kembali</a>
                             <button type="submit" class="btn btn-primary">Simpan</button>
                         </div>
@@ -347,7 +354,7 @@
                 resultA.appendChild(imgA);
                 document.querySelector('.save-foto-keluarga').classList.remove('hide');
                 document.querySelector('.options-foto-keluarga').classList.remove('hide');
-                cropperA = new Cropper(imgA);
+                cropperA = new Cropper(imgA, { zoomable: false } );
             }
             };
             readerA.readAsDataURL(e.target.files[0]);
@@ -356,14 +363,35 @@
         // save crop on click
         document.querySelector('.save-foto-keluarga').addEventListener('click',(e)=>{
             e.preventDefault();
-            let imgSrcA = cropperA.getCroppedCanvas({
-            }).toDataURL();
-            croppedA.src = imgSrcA;
-            // Ganti Value Input=File Foto Keluarga
-            $('#upload-img').attr("value", imgSrcA);
+            let imgSrcA = cropperA.getCroppedCanvas({}).toDataURL();
             $('#upload-img-text').val(imgSrcA);
+            document.querySelector('#data-satu').value = JSON.stringify( cropperA.getData() );
+            document.querySelector('#data-dua').value = JSON.stringify( cropperA.getCanvasData() );
+            document.querySelector('#result img').src = imgSrcA;
+            document.querySelector('#img-view').src = imgSrcA;
+            document.querySelector('#img-view').cropper.destroy();
         });
         // =============
         // FOTO KELUARGA
+
+        window.addEventListener('DOMContentLoaded', function () {
+            var datadiagonal = {"x":653.9289857784432,"y":596.4554640718563,"width":305.6724363772455,"height":204.74073727544913,"rotate":0,"scaleX":1,"scaleY":1} ;
+            var dataposisi = {"left":0,"top":0.25,"width":334,"height":250.5,"naturalWidth":1080,"naturalHeight":810} ;
+            var image = document.querySelector('#img-view');
+            var cropper = new Cropper(image, {
+                zoomable: false,
+                dragMode: false,
+                movable: false,
+                guides: false,
+                cropBoxResizable: false,
+                ready: function (event) {
+                    cropper.setData( datadiagonal );
+                    cropper.getCanvasData( dataposisi );                    
+                    var imagez = new Image();
+                    imagez.src = cropper.getCroppedCanvas({fillColor:'#fff'}).toDataURL('image/jpeg');
+                    document.getElementById('result').appendChild(imagez);
+                }
+            });
+        });
     </script>
 @endsection
